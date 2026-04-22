113c41a3766d3ea648d2673742294208dc8735c0
Jacintha Gardner <jacintha.gardner6@gmail.com>
2026-04-22 12:43:16 -0700

    added BufferedReader methods

---
+++ src/main/java/com/pluralsight/AdventureTime.java
@@ -0,0 +1,39 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
+public class AdventureTime
+{
+    public static void main(String[] args)
+    {
+        loadAdventure();
+    }
+
+    public static void loadAdventure()
+    {
+        {
+            try {
+                FileReader fileReader = new FileReader("adventure1.csv");
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+                String line = bufferedReader.readLine();
+
+                while (line != null) {
+                    System.out.println(line);
+                    line = bufferedReader.readLine();
+
+                }
+
+                fileReader.close();
+
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
+
